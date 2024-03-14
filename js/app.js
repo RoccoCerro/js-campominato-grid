02052d3f8d3abde0c580b2267d045e104ccc2bab
RoccoCerro <rcerro02@gmail.com>
2024-03-14 10:56:33 +0100

    REFACTOR

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-let size = 10;
-let numOfCell = size ** 2;
 const gridDomEl = document.querySelector(".grid");
 const buttonDomEl = document.querySelector("button");
 const selectDomEl = document.getElementById("select-difficulty");
 
-buttonDomEl.addEventListener("click", function(){
+startGame();
+
+buttonDomEl.addEventListener("click", startGame);
+
+function startGame(){
+    let size = getSize();
+    let numOfCell = size ** 2;
     const selectDomElValue = selectDomEl.value;
     
-    if(selectDomElValue === "medium"){
-        size = 9;
-        numOfCell = size ** 2;
-    }
-    else if(selectDomElValue === "easy"){
-        size = 7;
-        numOfCell = size ** 2;
-    }
+    gridDomEl.innerHTML = "";
+
+    // if(selectDomElValue === "medium"){
+    //     size = 9;
+    //     numOfCell = size ** 2;
+    // }
+    // else if(selectDomElValue === "easy"){
+    //     size = 10;
+    //     numOfCell = size ** 2;
+    // }
     
     for(let i = 0; i < numOfCell; i++){
         const num = i + 1;
@@ -22,28 +28,36 @@ buttonDomEl.addEventListener("click", function(){
         const cellEl = document.createElement("div");
         cellEl.classList.add("cell");
         cellEl.innerHTML = num;
+        cellEl.style.width = `calc(100% / ${size})`;
     
         gridDomEl.append(cellEl);
 
-        if(selectDomElValue === "medium"){
-            cellEl.classList.add("size-medium");
-        }
-        else if(selectDomElValue === "easy"){
-            cellEl.classList.add("size-easy");
-        }
+        // if(selectDomElValue === "medium"){
+        //     cellEl.classList.add("size-medium");
+        // }
+        // else if(selectDomElValue === "easy"){
+        //     cellEl.classList.add("size-hard");
+        // }
     
         cellEl.addEventListener("click", function(){
             console.log(num);
             cellEl.classList.toggle("bg-sky-blue");
         })
     }
-})
+}
 
-// function whereIsTheDifficulty(select){
-//     if(select === "medium"){
-//         return "medium"
-//     }
-//     else if(select === "easy"){
-//         return "easy"
-//     }
-// }
+function getSize(){
+    let size = 7;
+    const selectDomElValue = selectDomEl.value
+
+    if(selectDomElValue === "medium"){
+        size = 9;
+        numOfCell = size ** 2;
+    }
+    else if(selectDomElValue === "easy"){
+        size = 10;
+        numOfCell = size ** 2;
+    }
+
+    return size;
+}
